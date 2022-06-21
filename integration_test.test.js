7699af89b6d8e2d784c9852ce6d009afa34a6569
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-21 16:19:44 +0100

    test passing, modified structure and setup

--- integration_test.test.js
+++ integration_test.test.js
@@ -1,6 +1,7 @@
 jest.mock('./bankAccount');
 jest.mock('./transactions');
 jest.mock('./reports');
+jest.mock('./bankUI')
 
 // I wish I had something that would create a private bank account
 const BankAccount = require('./bankAccount');
@@ -11,16 +12,21 @@ const Transactions = require('./transactions');
 // I wish I had something that would allow for reporting from a bank account
 const Reports = require('./reports');
 
+// I wish I had a place to control the user interaction
+const BankUI = require('./bankUI');
+
 
 describe('setup', () => {
   it('creates instance of class bankAccount', () => {
+    const bankAccount = new BankAccount;
     const transactions = new Transactions;
     const reports = new Reports
-    const subject = new BankAccount(transactions,reports);
-
+    const subject = new BankUI(bankAccount, transactions, reports);
+    
+    expect(bankAccount).toBeInstanceOf(BankAccount);
     expect(transactions).toBeInstanceOf(Transactions);
     expect(reports).toBeInstanceOf(Reports);
-    expect(subject).toBeInstanceOf(BankAccount);
+    expect(subject).toBeInstanceOf(BankUI);
   })
 })
 
