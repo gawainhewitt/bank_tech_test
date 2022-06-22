b4913e29e12734563cf5d5705509ef175273da6e
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-22 17:04:31 +0100

    tests passing returns amount, date and balance when two deposits made

--- bankAccount.js
+++ bankAccount.js
@@ -1,8 +1,12 @@
 module.exports = class BankAccount {
-  
+  balance = 0;
+  #transactions = [];
+
   deposit(money, date) {
     this.money = money;
     this.date = date;
+    this.balance += money;
+    this.#transactions.push({date: this.date, credit: 0.00, deposit: this.money, balance: this.balance})
   }
 
   withdraw() {
@@ -10,13 +14,10 @@ module.exports = class BankAccount {
   }
 
   report() {
-    return {
-      date: this.date,
-      credit: 0.00,
-      deposit: this.money,
-      balance: this.money
-    }
+    console.log(this.#transactions);
+    return this.#transactions;
   };
+
 }
 
 //edge case of trying to withdraw more than the balance
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -5,32 +5,51 @@ describe('BankAccount', () => {
     it('returns amount, date and balance when deposit made', () => {
       const bankAccount = new BankAccount;
       bankAccount.deposit(1000.00, "23/02/2022");
-      expect(bankAccount.report()).toEqual({
+      expect(bankAccount.report()).toEqual([{
         date: "23/02/2022",
         credit: 0.00,
         deposit: 1000.00,
         balance: 1000.00
-      })
+      }])
     })
   })
   it('returns amount, date and balance when a different deposit made', () => {
     const bankAccount = new BankAccount;
     bankAccount.deposit(500.00, "23/02/2022");
-    expect(bankAccount.report()).toEqual({
+    expect(bankAccount.report()).toEqual([{
       date: "23/02/2022",
       credit: 0.00,
       deposit: 500.00,
       balance: 500.00
-    })
+    }])
   })
   it('returns amount, date and balance when a different amount and date given', () => {
     const bankAccount = new BankAccount;
     bankAccount.deposit(750.00, "24/02/2022");
-    expect(bankAccount.report()).toEqual({
+    expect(bankAccount.report()).toEqual([{
       date: "24/02/2022",
       credit: 0.00,
       deposit: 750.00,
       balance: 750.00
-    })
+    }])
+  })
+  it('returns amount, date and balance when two deposits made', () => {
+    const bankAccount = new BankAccount;
+    bankAccount.deposit(750.00, "24/02/2022");
+    bankAccount.deposit(123.19, "26/02/2022");
+    expect(bankAccount.report()).toEqual([
+      {
+      date: "24/02/2022",
+      credit: 0.00,
+      deposit: 750.00,
+      balance: 750.00
+      },
+      {
+      date: "26/02/2022",
+      credit: 0.00,
+      deposit: 123.19,
+      balance: 873.19
+      }
+    ])
   })
 })
