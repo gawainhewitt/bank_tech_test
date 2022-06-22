f66106bafbbedd4141a9850d7e01892b684107aa
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-22 16:17:22 +0100

    refactored transactions

--- transactions.js
+++ transactions.js
@@ -20,7 +20,6 @@ module.exports = class Transactions {
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     let yyyy = today.getFullYear();
-
     return dd + '/' + mm + '/' + yyyy;
   }
 
--- transactions.test.js
+++ transactions.test.js
@@ -12,31 +12,35 @@ describe('Transactions', () => {
   afterAll(() => {
     jest.useRealTimers()
   })
-  it("calls bankAccount.deposit with the money and the date when money deposited", () => {
-    const transactions = new Transactions;
-    const money = 300;
-    const bankAccount = new BankAccount;
-    transactions.deposit(money, bankAccount);
-    expect(bankAccount.deposit).toHaveBeenCalledWith(money.toFixed(2), "23/02/2022");
+  describe('.deposit', () => {
+    it("calls bankAccount.deposit with the money and the date when money deposited", () => {
+      const transactions = new Transactions;
+      const money = 300;
+      const bankAccount = new BankAccount;
+      transactions.deposit(money, bankAccount);
+      expect(bankAccount.deposit).toHaveBeenCalledWith(money.toFixed(2), "23/02/2022");
+    })
+    it("throws an error when a deposit is made with too many decimal places", () => {
+      const transactions = new Transactions;
+      expect(() => {
+        transactions.deposit(2000.627);
+      }).toThrow("too many decimal places");
+    })
   })
-  it("calls bankAccount.withdraw with the money and the date when money withdrawn", () => {
-    const transactions = new Transactions;
-    const money = 300;
-    const bankAccount = new BankAccount;
-    transactions.withdraw(money, bankAccount);
-    expect(bankAccount.withdraw).toHaveBeenCalledWith(money.toFixed(2), "23/02/2022");
-  })
-  it("throws an error when a deposit is made with too many decimal places", () => {
-    const transactions = new Transactions;
-    expect(() => {
-      transactions.deposit(2000.627);
-    }).toThrow("too many decimal places");
-  })
-  it("throws an error when a withdrawal is made with too many decimal places", () => {
-    const transactions = new Transactions;
-    expect(() => {
-      transactions.withdraw(500.627);
-    }).toThrow("too many decimal places");
+  describe('.withdraw', () => {
+    it("calls bankAccount.withdraw with the money and the date when money withdrawn", () => {
+      const transactions = new Transactions;
+      const money = 300;
+      const bankAccount = new BankAccount;
+      transactions.withdraw(money, bankAccount);
+      expect(bankAccount.withdraw).toHaveBeenCalledWith(money.toFixed(2), "23/02/2022");
+    })
+    it("throws an error when a withdrawal is made with too many decimal places", () => {
+      const transactions = new Transactions;
+      expect(() => {
+        transactions.withdraw(500.627);
+      }).toThrow("too many decimal places");
+    })
   })
 })
   
