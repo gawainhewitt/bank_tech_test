3b37271eb7342d013f44f8f53b06bc26fc9fe87c
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-22 17:45:44 +0100

    test passing for withdrawals

--- bankAccount.js
+++ bankAccount.js
@@ -3,14 +3,17 @@ module.exports = class BankAccount {
   #transactions = [];
 
   deposit(money, date) {
-    this.money = money;
-    this.date = date;
+    money = money;
+    date = date;
     this.balance += money;
-    this.#transactions.push({date: this.date, credit: this.money, debit: 0.00, balance: this.balance})
+    this.#transactions.push({date: date, credit: money, debit: 0.00, balance: this.balance})
   }
 
-  withdraw() {
-
+  withdraw(money, date) {
+    money = money;
+    date = date;
+    this.balance -= money;
+    this.#transactions.push({date: date, credit: 0.00, debit: money, balance: this.balance})
   }
 
   report() {
@@ -20,4 +23,4 @@ module.exports = class BankAccount {
 
 }
 
-//edge case of trying to withdraw more than the balance
+//edge case of trying to withdraw more than the balance?
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -51,5 +51,73 @@ describe('BankAccount', () => {
         }
       ])
     })
+    it('returns amount, date and balance when withdrawal made', () => {
+      const bankAccount = new BankAccount;
+      bankAccount.withdraw(1000.00, "23/02/2022");
+      expect(bankAccount.report()).toEqual([{
+        date: "23/02/2022",
+        credit: 0.00,
+        debit: 1000.00,
+        balance: -1000.00
+      }])
+    })
+    it('returns amount, date and balance when a different withdrawal made', () => {
+      const bankAccount = new BankAccount;
+      bankAccount.withdraw(500.00, "23/02/2022");
+      expect(bankAccount.report()).toEqual([{
+        date: "23/02/2022",
+        credit: 0.00,
+        debit: 500.00,
+        balance: -500.00
+      }])
+    })
+    it('returns amount, date and balance when a different amount and date given', () => {
+      const bankAccount = new BankAccount;
+      bankAccount.withdraw(750.00, "24/02/2022");
+      expect(bankAccount.report()).toEqual([{
+        date: "24/02/2022",
+        credit: 0.00,
+        debit: 750.00,
+        balance: -750.00
+      }])
+    })
+    it('returns amount, date and balance when two withdrawals made', () => {
+      const bankAccount = new BankAccount;
+      bankAccount.withdraw(750.00, "24/02/2022");
+      bankAccount.withdraw(123.19, "26/02/2022");
+      expect(bankAccount.report()).toEqual([
+        {
+        date: "24/02/2022",
+        credit: 0.00,
+        debit: 750.00,
+        balance: -750.00
+        },
+        {
+        date: "26/02/2022",
+        credit: 0.00,
+        debit: 123.19,
+        balance: -873.19
+        }
+      ])
+    })
+    it('returns amount, date and balance when one deposit and one withdrawal made', () => {
+      const bankAccount = new BankAccount;
+      bankAccount.deposit(750.00, "24/02/2022");
+      bankAccount.withdraw(123.19, "26/02/2022");
+      expect(bankAccount.report()).toEqual([
+        {
+        date: "24/02/2022",
+        credit: 750.00,
+        debit: 0.00,
+        balance: 750.00
+        },
+        {
+        date: "26/02/2022",
+        credit: 0.00,
+        debit: 123.19,
+        balance: 626.81
+        }
+      ])
+    })
   })
 })
